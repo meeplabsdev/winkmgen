90ab05f962880e7bac3ca4f7807bd2886ca11b64
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-12-13 21:57:10 +0000

    feat: Add handler for parenthesized_expression token

---
+++ src/entities/parenthesized_expression.rs
@@ -0,0 +1,14 @@
+use crate::entities::{Entity, ToRust};
+
+#[allow(unused)]
+pub struct ParenthesizedExpression<'a>(pub &'a Entity<'a>);
+impl<'a> ToRust<'a> for ParenthesizedExpression<'a> {
+    fn r(&'a self) -> Option<String> {
+        let children = &self.0.children;
+        if children.len() < 3 {
+            return None;
+        }
+
+        Some(format!("({})", children.get(1)?.r()?))
+    }
+}
