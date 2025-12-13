db9ccefbe4e9a18f8676816816bf92577b80be55
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-12-13 22:55:38 +0000

    feat: Update the login within call_expression to output the correct rust

--- src/entities/call_expression.rs
+++ src/entities/call_expression.rs
@@ -4,6 +4,17 @@ use crate::entities::{Entity, ToRust};
 pub struct CallExpression<'a>(pub &'a Entity<'a>);
 impl<'a> ToRust<'a> for CallExpression<'a> {
     fn r(&'a self) -> Option<String> {
-        Some(String::from("CALL"))
+        let children = &self.0.children;
+        if children.len() < 2 {
+            return None;
+        }
+
+        Some(
+            children
+                .iter()
+                .filter_map(|c| c.r())
+                .collect::<Vec<String>>()
+                .join(""),
+        )
     }
 }
