aafad56b479db0e8d3827aa9c0fcf57a320aab87
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-12-14 12:32:35 +0000

    feat: Update the logic within if_statement to output the correct rust

--- src/entities/if_statement.rs
+++ src/entities/if_statement.rs
@@ -4,6 +4,20 @@ use crate::entities::{Entity, ToRust};
 pub struct IfStatement<'a>(pub &'a Entity<'a>);
 impl<'a> ToRust<'a> for IfStatement<'a> {
     fn r(&'a self) -> Option<String> {
-        Some(String::from("IF"))
+        let children = &self.0.children;
+        if children.len() < 3 {
+            return None;
+        }
+
+        Some(format!(
+            "if {} {}",
+            children.get(1)?.r()?,
+            children
+                .get(2)?
+                .r()?
+                .lines()
+                .collect::<Vec<&str>>()
+                .join("\n\t")
+        ))
     }
 }
